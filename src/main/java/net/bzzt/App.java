21175f0a7a8c3f9512b9c266b5613e85f265a661
Arnout Engelen <arnout@bzzt.net>
2024-03-06 15:33:54 +0100

    initial code

---
+++ src/main/java/net/bzzt/App.java
@@ -0,0 +1,13 @@
+package net.bzzt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
